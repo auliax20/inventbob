844b2820c967fa0d01c4026826a5d8fa718cbc8d
Aulia Ulkhiari <auliaulkhairi@gmail.com>
2021-05-05 14:46:01 +0700

    tambah menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -26,6 +26,38 @@
                         {{ __('Karcis') }}
                     </x-jet-nav-link>
                 </div>
+                <div class="hidden sm:flex sm:items-center sm:ml-6 ">
+                    <div class="ml-3 relative">
+                        <x-jet-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                            <span class="inline-flex rounded-md">
+                                    <button type="button" class="inline-flex items-center px-3 py-2 h-auto border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                        Transaksi
+                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                </span>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Transaksi Karcis') }}
+                                </div>
+
+                                <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('Permintaan Karcis') }}
+                                </x-jet-dropdown-link>
+                                <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('Distribusi Karcis') }}
+                                </x-jet-dropdown-link>
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Laporan Karcis') }}
+                                </div>
+                            </x-slot>
+                        </x-jet-dropdown>
+                    </div>
+                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
